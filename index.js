0582ecbf95434d76da3ec042fe2d317dd5b6a4b9
dharti_dudhat <dhartii.dudhat@thegatewaycorp.co.in>
2024-07-05 19:00:13 +0530

    db connection

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("--->Hello world");
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("Blog", "root", "Harshil@1104", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("-----> DB connection has been established !");
+} catch (error) {
+  console.log("----> Unable to connect the db !");
+}
