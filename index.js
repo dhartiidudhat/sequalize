53f7f0e67bb17f197f39d56cce5cd98d263c9ab8
dharti_dudhat <dhartii.dudhat@thegatewaycorp.co.in>
2024-07-08 15:55:05 +0530

    remaning files

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-import { Sequelize } from "sequelize";
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { initialize } from "./src/config/db.js";
+import { logger } from "./src/config/logger.js";
 
-const sequelize = new Sequelize("Blog", "root", "Harshil@1104", {
-  host: "localhost",
-  dialect: "mysql",
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Hoem");
 });
 
-try {
-  await sequelize.authenticate();
-  console.log("-----> DB connection has been established !");
-} catch (error) {
-  console.log("----> Unable to connect the db !");
-}
+initialize()
+  .then(() => {
+    logger.info("Connected db on ", process.env.PORT);
+  })
+  .catch((err) => {
+    logger.error("errr", err);
+  });
+
+// ------------------>WOrking
+
+// import mysql from "mysql2/promise";
+// import { Sequelize } from "sequelize";
+
+// import dotenv from "dotenv";
+// import { logger } from "./src/config/logger.js";
+// import pkg from "winston";
+// const { info } = pkg;
+
+// dotenv.config();
+// const sequelize = new Sequelize("Blog", "root", "Harshil@11", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+// try {
+//   sequelize.authenticate().then(async () => {});
+// } catch (error) {
+//   console.log("second catch", error);
+// }
